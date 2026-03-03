2308a00a6ecb5d9189e0c5f73e6d957e3405a650
Shaye <shayelee25@gmail.com>
2026-03-03 12:22:18 -0500

    Create variables for all buttons

--- script.js
+++ script.js
@@ -19,13 +19,56 @@ const divide = function(a, b) {
 };
 //console.log(divide(60, 5))
 
-const firstNum = "";
-const operator = "";
-const secondNum = "";
+let firstNum = "";
+let operator = "";
+let secondNum = "";
+
 
 const operate = function() {
     return `${firstNum} ${operator} ${secondNum}`;
 };
+
+const displayTotal = document.querySelector("#.total");
+const addBtn = document.querySelector(".add");
+const subtractBtn = document.querySelector(".subtract");
+const multiplyBtn = document.querySelector(".multiply");
+const divideBtn = document.querySelector(".divide");
+const clearBtn = document.querySelector(".clear");
+const equalBtn = document.querySelector(".equal");
+let inputValue = "";
+displayTotal.appendChild(inputValue);
+
+const one = document.querySelector(".1");
+const two = document.querySelector(".2");
+const three = document.querySelector(".3");
+const four = document.querySelector(".4");
+const five = document.querySelector(".5");
+const six = document.querySelector(".6");
+const seven = document.querySelector(".7");
+const eight = document.querySelector(".8");
+const nine = document.querySelector(".9");
+const zero = document.querySelector(".0");
+const zeroZero = document.querySelector(".00");
+
+const allBtns = document.querySelectorAll("button");
+
+function reset(parent) {
+    while (parent.firstChild) {
+        parent.removeChild(parent.firstChild);
+    }
+}
+
+clearBtn.addEventListener("click", reset());
+
+allBtns.addEventListener("click", () => {
+    displayTotal.appendChild()
+})
+
+
+
+
+
+
 
 
 
